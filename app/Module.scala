07a706a1cd029897647e6cbedd16c5e800e951ef
Megan Juell <meganljuell@gmail.com>
2020-02-09 12:25:41 -0700

    binding on app startup for InitDb

--- app/Module.scala
+++ app/Module.scala
@@ -1,3 +1,11 @@
-class Module {
+import com.google.inject.AbstractModule
+import play.api.libs.concurrent.AkkaGuiceSupport
+import xyz.meggls.megglsxyz.db.InitDb
+
+class Module extends AbstractModule with AkkaGuiceSupport {
+
+    override def configure(): Unit = {
+        bind(classOf[InitDb]).asEagerSingleton()
+    }
 
 }
